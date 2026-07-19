c215d9c4c66cf56959bdbb0bf969f986ad3a2eac
ssathyasai <sathyasai1357@gmail.com>
2026-07-19 11:41:08 +0530

    config-module-exports

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,8 @@
-"""Configuration package for the chatbot application."""
+"""Configuration package initialization.
+
+Process Flow:
+1. Loads and exposes application-wide Pydantic configuration settings.
+"""
 
 from .settings import settings
 
