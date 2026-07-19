467ac31e67012c2d896998d371ecb9b31d7f10b0
ssathyasai <sathyasai1357@gmail.com>
2026-07-19 11:42:25 +0530

    document-module-exports

--- document/__init__.py
+++ document/__init__.py
@@ -1,4 +1,9 @@
-"""Document processing package."""
+"""Document processing package initialization.
+
+Process Flow:
+1. Exports high-level `DocumentProcessor` entry point.
+2. Exports `Document` and `DocumentChunk` data model classes.
+"""
 
 from .processor import DocumentProcessor
 from .models import Document, DocumentChunk
