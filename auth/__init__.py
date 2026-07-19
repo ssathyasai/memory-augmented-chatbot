da7ff6324f7b749892b43d480a250da5ee47dedb
ssathyasai <sathyasai1357@gmail.com>
2026-07-19 11:39:29 +0530

    authentication-module-exports

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,4 +1,9 @@
-"""Authentication package."""
+"""Authentication package initialization.
+
+Process Flow:
+1. Exports core authentication entities (AuthManager).
+2. Exports Pydantic schemas for authentication requests and responses (User, UserCreate, UserLogin, UserResponse).
+"""
 
 from .manager import AuthManager
 from .models import User, UserCreate, UserLogin, UserResponse
