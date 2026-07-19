a7f0e6ad3330b52d8c44f65038dc200ad1f338a3
ssathyasai <sathyasai1357@gmail.com>
2026-07-19 11:44:47 +0530

    knowledge-graph-exports

--- knowledge_graph/__init__.py
+++ knowledge_graph/__init__.py
@@ -1,4 +1,9 @@
-"""Knowledge graph package."""
+"""Knowledge graph package initialization.
+
+Process Flow:
+1. Exports factory function `get_knowledge_graph` for obtaining user-scoped graph instances.
+2. Exports `Neo4jKnowledgeGraph` driver manager class.
+"""
 
 from .manager import get_knowledge_graph
 from .neo4j_manager import Neo4jKnowledgeGraph
