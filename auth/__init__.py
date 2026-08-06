38264fed324fc46d332c35a1c2c123557068dba3
ssathyasai <sathyasai1357@gmail.com>
2026-08-06 21:47:24 +0530

    Fix circular import KeyError in auth module

--- auth/__init__.py
+++ auth/__init__.py
@@ -5,7 +5,7 @@ Process Flow:
 2. Exports Pydantic schemas for authentication requests and responses (User, UserCreate, UserLogin, UserResponse).
 """
 
-from .manager import AuthManager
 from .models import User, UserCreate, UserLogin, UserResponse
+from .manager import AuthManager
 
 __all__ = ["AuthManager", "User", "UserCreate", "UserLogin", "UserResponse"]
